737ca430699f433f8652034e8a48e86a9e166e3f
Foo wai Chong <stevengo123@gmail.com>
2021-12-25 03:14:42 -0500

    test-10

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,10 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  function range(x){
+  return this[0] <= x && x <= this[1]
+  }
+  let count = nums.filter(range, [start, end]).length;
+  return count;
 }
 
 // Do not edit this line;
