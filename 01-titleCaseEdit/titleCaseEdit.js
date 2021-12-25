32898a5aca1a33ebe85286553de42953de01cd4a
Foo wai Chong <stevengo123@gmail.com>
2021-12-25 00:29:50 -0500

    test-01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,14 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let titleHolder = title;
+  let stringHolder = titleHolder.split(" ");
+  for (let i = 0; i <stringHolder.length; i++){
+    stringHolder[i] = stringHolder[i].charAt(0).toUpperCase() +
+    stringHolder[i].slice(1);
+  }
+
+  let stringTogether = stringHolder.join(" ");
+  return stringTogether;
 }
 
 // Do not edit this line;
