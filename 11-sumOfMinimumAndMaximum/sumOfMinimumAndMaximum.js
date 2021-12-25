83b3e45ae98e562de25fb8f3b148f051cc209e43
Foo wai Chong <stevengo123@gmail.com>
2021-12-25 03:37:35 -0500

    test-11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,14 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  function max(array){
+    return Math.max.apply(null, array);
+  }
+  function min(array){
+    return Math.min.apply(null, array);
+  }
+  let sum = max(nums) + min(nums);
+
+  return sum;
 }
 
 // Do not edit this line;
