c062368bb1c0f827a5e1afbe4b6fdf085a9ff523
Foo wai Chong <stevengo123@gmail.com>
2021-12-25 19:59:09 -0500

    test-14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,6 @@
 function frequencyCounter(word) {
   // Insert code here;
-  let stringArr = word.split('');
+  let stringArr = word.split(''); //creates an array of chars
   let frequency = stringArr.reduce((key, value) => {
     key[value] = (key[value] ?? 0) + 1; //if current key[value] is null use 0 then + 1;
     return key;
