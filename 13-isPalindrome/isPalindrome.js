4ff1c7a70705df3dddfd8f2bfc0438a23076ed31
Foo wai Chong <stevengo123@gmail.com>
2021-12-25 19:36:13 -0500

    test-13

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,8 @@
 function isPalindrome(word) {
   // Insert code here;
+  let reverseString = word.toLowerCase().split('').reverse().join('');
+  let lowerCaseWord = word.toLowerCase();
+  return (lowerCaseWord == reverseString);
 }
 
 // Do not edit this line;
