7be66d0e6055809e31a2b9e68ff9a7099daa57b6
Foo wai Chong <stevengo123@gmail.com>
2021-12-26 17:48:11 -0500

    test-19

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,19 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    let mid = Math.floor(nums.length /2);
+    if (nums.length == 1 && nums[0] != target){
+      return false;
+    }
+    if (mid == target){
+      return true;
+    }
+    if(nums[mid] > target){
+      return this.binarySearch (nums.slice(0, mid), target);
+    } else if (nums[mid] < target){
+      return this.binarySearch (nums.slice(mid), target);
+    }
+    return true;
   }
 }
 
