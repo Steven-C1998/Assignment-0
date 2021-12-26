24cd3d6a6ea9d8144a129e4d99b418df214eeceb
Foo wai Chong <stevengo123@gmail.com>
2021-12-25 21:43:10 -0500

    test-16

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,11 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  let product = arguments[0];
+  for (let i = 1; i < arguments.length;i++){
+    product *= arguments[i];
+  }
+
+  return product;
 }
 
 // Do not edit this line;
