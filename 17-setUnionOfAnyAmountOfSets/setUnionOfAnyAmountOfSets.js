e6d79d3bab1b872dd187b1326d40a1f7cd5302b5
Foo wai Chong <stevengo123@gmail.com>
2021-12-25 22:18:32 -0500

    test-17

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  
+  let union = arguments[0];
+  for (let i = 1; i < arguments.length;i++){
+    union = new Set([...union,...arguments[i]]);
+  }
+  
+  
+  return union;
 }
 
 // Do not edit this line;
